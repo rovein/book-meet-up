9fafe26e38a18894dd24714e6fc2ecd2b9e59600
Kuznetsov, Roman <Roman.Kuznetsov@wolterskluwer.com>
2022-05-11 00:01:02 +0300

    Frontend. Established header, SignUp form and edit profile form

---
+++ book-meet-up-frontend/src/View/Components/auth/HeaderAuth.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import {useTranslation, withTranslation} from 'react-i18next';
+import {ADMIN, EMPLOYEE} from "../util/Constants";
+import {
+    checkToken,
+    clearLocalStorage,
+    getCurrentLanguage,
+    getCurrentUserRole,
+    setCurrentLanguage
+} from "../util/LocalStorageUtils";
+import {determineInitialLanguage, onLanguageHandle} from "../util/LocalizationUtil";
+
+function Header(props) {
+
+    checkToken()
+
+    const [language, setLanguage] = useState(determineInitialLanguage());
+
+    const signOut = _ => {
+        const language = getCurrentLanguage();
+        clearLocalStorage();
+        setCurrentLanguage(language)
+        window.location.href = '/';
+    }
+
+    const {t} = useTranslation();
+    return (
+        <div className="header">
+            <nav>
+                <ul className="nav_links">
+                    <li><input type="button" id="locale" value={language}
+                               onClick={() => onLanguageHandle(language, setLanguage, props.i18n)}/></li>
+                    <li><a href="/profile">{t("Profile")}</a></li>
+                    {getCurrentUserRole() === EMPLOYEE && <>
+                        <li><a href="/edit">{t('EditP')}</a></li>
+                        <li><a href="/create-booking">{t('CreateBooking')}</a></li>
+                    </>}
+                    {getCurrentUserRole() === ADMIN && <>
+                        <li><a id="BC">{t("Backup")}</a></li>
+                    </>}
+                    <li><a onClick={signOut} id="SO">{t("Signout")}</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default withTranslation()(Header);
---
+++ book-meet-up-frontend/src/View/Components/auth/SignUpForm.js
@@ -0,0 +1,210 @@
+import React from 'react'
+import Input from '../ui/Input'
+import Button from '../ui/Button'
+import {withTranslation} from 'react-i18next'
+import * as Constants from "../util/Constants";
+import {EMPLOYEE} from "../util/Constants";
+import {authInstance} from '../util/ApiUtil';
+import DefaultLoader from "../ui/Loader";
+
+class SignUpForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPass: '',
+            phone: '',
+            flag: 1,
+            buttonDisabled: false,
+            isLoading: false
+        }
+    }
+
+    setInputValue(property, val) {
+        this.setState({
+            [property]: val
+        })
+    }
+
+    redirectRes() {
+        if (this.props.role === EMPLOYEE) {
+            this.doSignUp(`/auth/register/employee`);
+        }
+    }
+
+    checkEmail(email) {
+        let regEmail = new RegExp('^([a-z0-9_-]+.)*[a-z0-9_-]+@[a-z0-9_-]+(.[a-z0-9_-]+)*.[a-z]{2,6}$');
+        if (!regEmail.test(email)) {
+            this.setState({flag: 2});
+            return false
+        }
+        return true
+    }
+
+    checkName(name) {
+        let regName = new RegExp('^([А-ЯЁа-яё0-9]+)|([A-Za-z0-9]+)$');
+        if (!regName.test(name)) {
+            this.setState({flag: 4});
+            return false
+        }
+        return true
+    }
+
+    checkPhone(phone) {
+        let regPhone = new RegExp('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$');
+        if (!regPhone.test(phone)) {
+            this.setState({flag: 5});
+            return false
+        }
+        return true
+    }
+
+    checkPass(password) {
+        if (password.length < 8) {
+            this.setState({flag: 11});
+            return false
+        }
+        return true
+    }
+
+    checkPasswords(password, confirmPassword) {
+        if (password !== confirmPassword) {
+            this.setState({flag: 12});
+            return false
+        }
+        return true
+    }
+
+    checkCred() {
+        if (!this.checkName(this.state.firstName)) {
+            return
+        }
+        if (!this.checkName(this.state.lastName)) {
+            return
+        }
+        if (!this.checkEmail(this.state.email)) {
+            return
+        }
+        if (!this.checkPhone(this.state.phone)) {
+            return
+        }
+        if (!this.checkPass(this.state.password)) {
+            return
+        }
+        if (!this.checkPasswords(this.state.password, this.state.confirmPass)) {
+            return
+        }
+
+        this.setState({
+            buttonDisabled: true,
+            isLoading: true
+        })
+
+        this.redirectRes()
+    }
+
+    async doSignUp(resUrl) {
+        try {
+            let res = await authInstance.post(resUrl, {
+                email: this.state.email,
+                phoneNumber: this.state.phone,
+                password: this.state.password,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                role: this.props.role,
+            })
+            let result = res.data
+            if (result && result.id !== null) {
+                window.location.href = './login';
+            }
+        } catch (e) {
+            const result = e.response;
+            if (result.status === 400 && result.data.email) {
+                this.setState({flag: 10, buttonDisabled: false, isLoading: false});
+            }
+        }
+    }
+
+    render() {
+        const {t} = this.props
+        const inputClass = Constants.INPUT_STYLE_CLASSES;
+        if (this.state.isLoading) {
+            return <DefaultLoader height={425} width={425} isCentered={false}/>
+        }
+        return (
+            <div
+                className="w3-container w3-card-4 w3-light-grey w3-text-indigo w3-margin"
+                style={{width: "700px"}}>
+                <h1 className="w3-center">{t('Signup')}</h1>
+                <div className="sized-font w3-center w3-text-red">
+                    {this.state.flag === 2 && <span>{t("EEmail")}</span>}
+                    {this.state.flag === 4 && <p>{t("EName")}</p>}
+                    {this.state.flag === 5 && <p>{t("EPhone")}</p>}
+                    {this.state.flag === 10 && <p>{t("eExist")}</p>}
+                    {this.state.flag === 11 && <p>{t("EPass")}</p>}
+                    {this.state.flag === 12 && <p>{t("EConfirmPass")}</p>}
+                </div>
+
+                <Input
+                    className={this.state.flag === 4 ? inputClass + " w3-border-red" : inputClass}
+                    type='text'
+                    placeholder={t('FirstName')}
+                    value={this.state.name ? this.state.name : ''}
+                    onChange={(val) => this.setInputValue('firstName', val)}
+                />
+
+                <Input
+                    className={this.state.flag === 4 ? inputClass + " w3-border-red" : inputClass}
+                    type='text'
+                    placeholder={t('LastName')}
+                    value={this.state.name ? this.state.name : ''}
+                    onChange={(val) => this.setInputValue('lastName', val)}
+                />
+
+                <Input
+                    className={this.state.flag === 2 ? inputClass + " w3-border-red" : inputClass}
+                    type='text'
+                    placeholder={t('Email')}
+                    value={this.state.email ? this.state.email : ''}
+                    onChange={(val) => this.setInputValue('email', val)}
+                />
+
+                <Input
+                    className={this.state.flag === 5 ? inputClass + " w3-border-red" : inputClass}
+                    type='text'
+                    placeholder={t('Phone')}
+                    value={this.state.phone ? this.state.phone : ''}
+                    onChange={(val) => this.setInputValue('phone', val)}
+                />
+
+                <Input
+                    className={(this.state.flag === 11 || this.state.flag === 12) ? inputClass + " w3-border-red" : inputClass}
+                    type='password'
+                    placeholder={t('Password')}
+                    value={this.state.password ? this.state.password : ''}
+                    onChange={(val) => this.setInputValue('password', val)}
+                />
+
+                <Input
+                    className={this.state.flag === 12 ? inputClass + " w3-border-red" : inputClass}
+                    type='password'
+                    placeholder={t('ConfirmPassword')}
+                    value={this.state.confirmPass ? this.state.confirmPass : ''}
+                    onChange={(val) => this.setInputValue('confirmPass', val)}
+                />
+
+                <Button
+                    className="w3-btn w3-block w3-section w3-indigo w3-padding"
+                    text={t('Signup')}
+                    disabled={this.state.buttonDisabled}
+                    onClick={() => this.checkCred()}
+                />
+            </div>
+        )
+    }
+}
+
+export default withTranslation()(SignUpForm);
